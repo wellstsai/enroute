aecc554d2cad8aca73194fcaaf6930dfc88470f4
Wells Tsai <wells.tsai@tempus.com>
2018-02-19 23:00:32 -0600

    wip

---
+++ src/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+
+const app = express();
+const port = 8080;
+
+app.use(helmet());
+app.use(express.static(path.join(__dirname, '../../dist')));
+
+app.listen(port, () => {
+  console.log(`En Route server listening on port ${port}`);
+});
